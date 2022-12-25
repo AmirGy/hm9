3a2837d8007cdcd8752d28b434c8445218af872f
Amir <firstharrypotter1@gmail.com>
2022-12-25 18:03:35 +0600

    solved second task

---
+++ zad2/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Write("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write(SumNumbersRec(M, N));
+
+// int SumNumbers(int M, int N)
+// {
+//     int res = 0;
+//     while (true)
+//     {
+//         res+=N;
+//         N--;
+//         if (N == M-1)
+//             break;
+//     }
+//     return res;
+// }
+
+int SumNumbersRec(int M, int N)
+{
+    if(N==M-1) return 0;
+    return N+SumNumbersRec(M, N-1);
+}
